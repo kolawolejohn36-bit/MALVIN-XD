d01a079a7c9575655da6e5d78a7eebd1c927d518
kolawolejohn36-bit <kolawolejohn36@gmail.com>
2025-08-28 11:58:20 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "malvin~Yu0AUYDA#ciK0Wjjo2H8PP1c3H16yIPTiA1afEV3fBHd_xV7S_W4",  // add your Session Id here
+  SESSION_ID: "malvin~Yn12VbhD#bDU0RPpJhcNRNF_X7qREK6D09CBdBuejD-fkxVL_mcg",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
